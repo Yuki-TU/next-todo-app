eaf54c0eda663e4b8b5869050156cbec5933479b
Yuki-TU <k.yuki.90219@icloud.com>
2022-06-02 02:17:40 +0900

    chakraUIを使う設定

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,12 +1,15 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 import apolloClient from "../lib/apollo";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ApolloProvider>
   );
 }
