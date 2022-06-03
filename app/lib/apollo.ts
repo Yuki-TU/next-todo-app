a90f8bd1366a043b560d941f9201eba5b4b3d914
Yuki-TU <k.yuki.90219@icloud.com>
2022-06-04 03:15:21 +0900

    環境変数の設定

--- app/lib/apollo.ts
+++ app/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:3001/api/graphql", // エンドポイント設定
+  uri: process.env.NEXT_PUBLIC_TODO_API_URL, // エンドポイント設定
   cache: new InMemoryCache(), // キャッシュ設定
 });
 
